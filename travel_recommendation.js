630f403b8e6672e449389c49122e5c5325ab700d
stevecancode <scarrillo57@gmail.com>
2024-05-30 15:10:10 -0400

    final

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,38 +12,34 @@ function searchCondition() {
         .then(response => response.json())
 
         .then(data => {
-                
-            for (keyword in data){
-
-                if (input === 'countries'){
-                    const countriesLength = data.countries.length;
-                    for (let i = 0; i < countriesLength; ++i) {
-                        let citiesLength = data.countries[i].cities.length;
-                        for (let y = 0; y < citiesLength; ++y) {
-                            resultDiv.innerHTML += `<h2>${data.countries[i].cities[y].name}</h2>`;
-                            resultDiv.innerHTML += `<img src="${data.countries[i].cities[y].imageUrl}" alt="hjh">`;
-                            resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.countries[i].cities[y].description}</p>`;
-                        }
+            if (input === 'countries'){
+                const countriesLength = data.countries.length;
+                for (let i = 0; i < countriesLength; ++i) {
+                    let citiesLength = data.countries[i].cities.length;
+                    for (let y = 0; y < citiesLength; ++y) {
+                        resultDiv.innerHTML += `<h2>${data.countries[i].cities[y].name}</h2>`;
+                        resultDiv.innerHTML += `<img src="${data.countries[i].cities[y].imageUrl}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.countries[i].cities[y].description}</p>`;
                     }
-                } else if (input === 'temples') {
-                    const templesLength = data.temples.length;
-                    for (let i = 0; i < templesLength; ++i) {
-                        resultDiv.innerHTML += `<h2>${data.temples[i].name}</h2>`;
-                        resultDiv.innerHTML += `<img src="${data.temples[i].imageUrl}" alt="hjh">`;
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.temples[i].description}</p>`;
-                    }    
-
-                } else if (input === 'beaches') {
-                    const beachesLength = data.beaches.length;
-                    for (let i = 0; i < beachesLength; ++i) {
-                        resultDiv.innerHTML += `<h2>${data.beaches[i].name}</h2>`;
-                        resultDiv.innerHTML += `<img src="${data.beaches[i].imageUrl}" alt="hjh">`;
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.beaches[i].description}</p>`;
-                    }
-
-                } else {
-                    resultDiv.innerHTML += `<h2>Destination or keyword not found.</h2>`;
                 }
+            } else if (input === 'temples') {
+                const templesLength = data.temples.length;
+                for (let i = 0; i < templesLength; ++i) {
+                    resultDiv.innerHTML += `<h2>${data.temples[i].name}</h2>`;
+                    resultDiv.innerHTML += `<img src="${data.temples[i].imageUrl}" alt="hjh">`;
+                    resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.temples[i].description}</p>`;
+                }    
+
+            } else if (input === 'beaches') {
+                const beachesLength = data.beaches.length;
+                for (let i = 0; i < beachesLength; ++i) {
+                    resultDiv.innerHTML += `<h2>${data.beaches[i].name}</h2>`;
+                    resultDiv.innerHTML += `<img src="${data.beaches[i].imageUrl}" alt="hjh">`;
+                    resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.beaches[i].description}</p>`;
+                }
+
+            } else {
+                resultDiv.innerHTML += `<h2>Destination or keyword not found.</h2>`;
             }
         })
         .catch(error => {
