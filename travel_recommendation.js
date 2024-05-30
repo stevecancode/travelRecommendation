e0b7e491f59cca8eb0bac4d31342fd0aabf0e059
stevecancode <scarrillo57@gmail.com>
2024-05-30 12:19:25 -0400

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -12,16 +12,37 @@ function searchCondition() {
         .then(response => response.json())
 
         .then(data => {
-            for (keyword in data) {
-                if (keyword === input) {
-                    for (country in keyword) {
-                        resultDiv.innerHTML += `<h2>${data.keyword.country.cities}</h2>`;
-                        /*resultDiv.innerHTML += `<img src="${country.cities.imageUrl}" alt="hjh">`;*/
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.keyword.country.cities.description}</p>`;
+                
+            for (keyword in data){
+
+                if (input === 'countries'){
+                    for (country in data.countries) {
+                        resultDiv.innerHTML += `<h2>${Object.entries(data.countries[0])}</h2>`;
+                        /*resultDiv.innerHTML += `<img src="${country.cities.imagesrc}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${contry.cities.description}</p>`;*/
                     }
-                } 
+
+                } else if (input === 'temples') {
+                    const lenTemples = data.temples.length;
+                    for (let i = 0; i < lenTemples; ++i) {
+                        resultDiv.innerHTML += `<h2>${temple.name}</h2>`;
+                        resultDiv.innerHTML += `<img src="${temple.imageUrl}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${temple.description}</p>`;
+                    };
+                } else if (input === 'beaches') {
+                    const lenBeaches = data.temples.length;
+                    for (let i = 0; i < lenBeaches; ++i) {
+                        resultDiv.innerHTML += `<h2>${beach.name}</h2>`;
+                        resultDiv.innerHTML += `<img src="${beach.imageUrl}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${beach.description}</p>`;
+                    };
+                } else {
+                    resultDiv.innerHTML += `<h2>Destination or keyword not found.</h2>`;
+                }
+
+
             }
-            
+            resultDiv.innerHTML += `<p><strong>Description:</strong> ${Object.entries(data.temples[0])}</p>`;
             
             /*const countryObj = data.countries.find(item => item.countries.name.toLowerCase() === input);
             const beachObj = data.beaches.find(item => item.countriesname.toLowerCase() === input);
