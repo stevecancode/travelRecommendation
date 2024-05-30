a35a20f72cc31be5e4b2cec7cec19e33d1a7d7eb
stevecancode <scarrillo57@gmail.com>
2024-05-30 14:50:03 -0400

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -16,49 +16,35 @@ function searchCondition() {
             for (keyword in data){
 
                 if (input === 'countries'){
-                    for (country in data.countries) {
-                        resultDiv.innerHTML += `<h2>${Object.entries(data.countries[0])}</h2>`;
-                        /*resultDiv.innerHTML += `<img src="${country.cities.imagesrc}" alt="hjh">`;
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${contry.cities.description}</p>`;*/
+                    const countriesLength = data.countries.length;
+                    for (let i = 0; i < countriesLength; ++i) {
+                        let citiesLength = data.countries[i].cities.length;
+                        for (let y = 0; y < citiesLength; ++y) {
+                            resultDiv.innerHTML += `<h2>${data.countries[i].cities[y].name}</h2>`;
+                            resultDiv.innerHTML += `<img src="${data.countries[i].cities[y].imageUrl}" alt="hjh">`;
+                            resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.countries[i].cities[y].description}</p>`;
+                        }
+                    }
+                } else if (input === 'temples') {
+                    const templesLength = data.temples.length;
+                    for (let i = 0; i < templesLength; ++i) {
+                        resultDiv.innerHTML += `<h2>${data.temples[i].name}</h2>`;
+                        resultDiv.innerHTML += `<img src="${data.temples[i].imageUrl}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.temples[i].description}</p>`;
+                    }    
+
+                } else if (input === 'beaches') {
+                    const beachesLength = data.beaches.length;
+                    for (let i = 0; i < beachesLength; ++i) {
+                        resultDiv.innerHTML += `<h2>${data.beaches[i].name}</h2>`;
+                        resultDiv.innerHTML += `<img src="${data.beaches[i].imageUrl}" alt="hjh">`;
+                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${data.beaches[i].description}</p>`;
                     }
 
-                } else if (input === 'temples') {
-                    const lenTemples = data.temples.length;
-                    for (let i = 0; i < lenTemples; ++i) {
-                        resultDiv.innerHTML += `<h2>${temple.name}</h2>`;
-                        resultDiv.innerHTML += `<img src="${temple.imageUrl}" alt="hjh">`;
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${temple.description}</p>`;
-                    };
-                } else if (input === 'beaches') {
-                    const lenBeaches = data.temples.length;
-                    for (let i = 0; i < lenBeaches; ++i) {
-                        resultDiv.innerHTML += `<h2>${beach.name}</h2>`;
-                        resultDiv.innerHTML += `<img src="${beach.imageUrl}" alt="hjh">`;
-                        resultDiv.innerHTML += `<p><strong>Description:</strong> ${beach.description}</p>`;
-                    };
                 } else {
                     resultDiv.innerHTML += `<h2>Destination or keyword not found.</h2>`;
                 }
-
-
             }
-            resultDiv.innerHTML += `<p><strong>Description:</strong> ${Object.entries(data.temples[0])}</p>`;
-            
-            /*const countryObj = data.countries.find(item => item.countries.name.toLowerCase() === input);
-            const beachObj = data.beaches.find(item => item.countriesname.toLowerCase() === input);
-            const templeObj = data.temples.find(item => item.countries.name.toLowerCase() === input);
-            
-            if (country) {
-            
-                for (country in countryObj) {
-                    resultDiv.innerHTML += `<h2>${country.cities}</h2>`;
-                    resultDiv.innerHTML += `<img src="${country.cities.imagesrc}" alt="hjh">`;
-                    resultDiv.innerHTML += `<p><strong>Description:</strong> ${contry.cities.description}</p>`;
-                }
-            } else {
-            resultDiv.innerHTML = 'Destination or keyword not found.';
-            }*/
-
         })
         .catch(error => {
         console.error('Error:', error);
